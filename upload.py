ee760e38b3becc1608ae39753603212c5c1925e6
siIverfish <ivan.g.zimmerman@gmail.com>
2023-04-19 21:01:13 -0700

    finalizing

--- upload.py
+++ upload.py
@@ -5,69 +5,15 @@ import string
 
 from typing import Any
 
-DEFAULT_HOST = "http://127.0.0.1:3000"
-CREATE_AUTO_PATH = "/ai_models/create_auto"
+from error_helpers import (
+    token_error,
+    name_error,
+    description_error,
+    score_error,
+    hostname_error,
+)
 
-TOKEN_LENGTH = 16
-BASE62 = string.ascii_letters + string.digits
-
-NAME_MIN_LENGTH = 3
-NAME_MAX_LENGTH = 30
-
-DESCRIPTION_MIN_LENGTH = 5
-DESCRIPTION_MAX_LENGTH = 400
-
-# ----------------------- error detection helper functions -----------------------
-
-def token_error(token: str) -> bool:
-    if len(token) != 16 or any(character not in BASE62 for character in token):
-        print(f"Error: invalid token {token!r}")
-        print(f"Token should be 16 characters (not {len(token)}) and every character should be in this set:")
-        print(BASE62 + '\n')
-        return True
-    return False
-
-def name_error(name: str) -> bool:
-    if name is None:
-        print("Warning: name is None\n")
-        return False
-    if not NAME_MIN_LENGTH <= len(name) <= NAME_MAX_LENGTH:
-        print(f"Error: name {name!r} of bad length {len(name)}")
-        print(f"Minimum is {NAME_MIN_LENGTH} and maximum is {NAME_MAX_LENGTH}\n")
-        return True
-    return False
-
-def description_error(description: str) -> bool:
-    if description is None:
-        print("Warning: description is None\n")
-        return False
-    if not DESCRIPTION_MIN_LENGTH <= len(description) <= DESCRIPTION_MAX_LENGTH:
-        print(f"Error: description {description!r} of bad length {len(description)}")
-        print(f"Minimum is {DESCRIPTION_MIN_LENGTH} and maximum is {DESCRIPTION_MAX_LENGTH}\n")
-        return True
-    return False
-
-def score_error(score: float) -> bool:
-    if not 0 <= score <= 1:
-        print("Invalid score! (nice try)")
-        print(f"Score {score!r} not between 0 and 1\n")
-        return True
-    return False
-
-def hostname_error(hostname: str) -> bool:
-    if not hostname.startswith("http"):
-        print("Error: bad hostname {hostname!r} does not start with 'http'")
-        print(f"Did you mean: {'https://' + hostname!r}\n")
-        return True
-    if hostname.endswith("/"):
-        print("Error: bad hostname {hostname!r} ends with '/'")
-        print(f"Did you mean: {hostname.removeprefix('/')!r}\n")
-        return True
-    return False
-
-# ----------------------- real function -----------------------
-
-def upload_model(model: Any, /, *, token: str, name: str=None, 
+def upload_model(model: Any, *, token: str, name: str=None, 
                 description: str=None, score: float=None, hostname: str=None) -> None:
     if hostname is None:
         print("Warning: No hostname provided!")
